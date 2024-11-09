3b6ceca99c1ebae49eb7475bb1716b71fcace2c6
IvanRecio <irecio@m-shop.mobi>
2024-11-09 16:40:22 +0100

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
  // Aquí tu código
+var btnAgregar = document.getElementById("agregar")
+var list = document.getElementById("lista")
+
+btnAgregar.addEventListener('click', () => {
+    var elementList = prompt("Ingrese un elemento a la lista:")
+    if(elementList != ""){
+        list.innerHTML += "<li>" + elementList + "</li>"
+        alert("Su elemento se ha añadido correctamente")
+    }else{
+        alert("Tienes que ingreasar algo")
+    }
+})
